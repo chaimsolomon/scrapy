24ca5a14b31f2956e0de84a2deea3b749840b559
Chaim Solomon <chaim@itcentralstation.com>
2014-03-18 14:59:36 +0200

    Added RabbitQueue and and use module for queue name

--- scrapy/squeue.py
+++ scrapy/squeue.py
@@ -6,13 +6,60 @@ import marshal, cPickle as pickle
 
 from queuelib import queue
 
+import pika
+
+class RabbitQueue(object):
+    """Queue that uses RabbitMQ with Pika for the backend"""
+
+    def __init__(self, queuename="default"):
+        self.queuename = queuename
+        self.seen_queues = []
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue=self.queuename)
+        self.seen_queues.append(self.queuename)
+
+    def push(self, string):
+        # print "Pushed %s" % string
+        self.channel.basic_publish(exchange='',
+                                   routing_key=self.queuename,
+                                   body=string)
+
+    def push_to_queue(self, string, queue):
+        if queue not in self.seen_queues:
+            self.channel.queue_declare(queue=queue)
+            self.seen_queues.append(queue)
+        self.channel.basic_publish(exchange='',
+                                   routing_key=queue,
+                                   body=string)
+
+    def pop(self, queuename=None):
+        qn = queuename or self.queuename
+        method_frame, properties, body = self.channel.basic_get(queue=qn)
+        if method_frame:
+            self.channel.basic_ack(method_frame.delivery_tag)
+        return body
+
+    def __len__(self):
+        return self.channel.queue_declare(self.queuename).method.message_count
+
+    def close(self):
+        self.channel.basic_cancel()
+        self.channel.close()
+        self.connection.close()
+
 def _serializable_queue(queue_class, serialize, deserialize):
 
     class SerializableQueue(queue_class):
 
         def push(self, obj):
+            # code.interact('push', local=locals())
             s = serialize(obj)
-            super(SerializableQueue, self).push(s)
+            parent = super(SerializableQueue, self)
+            if obj.callback and hasattr(parent, 'push_to_queue') and callable(parent.push_to_queue):
+                parent.push_to_queue(s, obj.callback.im_self.__class__.__module__)
+            else:
+                super(SerializableQueue, self).push(s)
 
         def pop(self):
             s = super(SerializableQueue, self).pop()
@@ -37,3 +84,5 @@ MarshalLifoDiskQueue = _serializable_queue(queue.LifoDiskQueue, \
     marshal.dumps, marshal.loads)
 FifoMemoryQueue = queue.FifoMemoryQueue
 LifoMemoryQueue = queue.LifoMemoryQueue
+
+SerializedRabbitQueue = _serializable_queue(RabbitQueue, _pickle_serialize, pickle.loads)
